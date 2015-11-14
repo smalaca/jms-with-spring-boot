dc19b90e3422a781877c2176712fd0202d9ac613
smalaca <ab13.krakow@gmail.com>
2015-11-14 08:19:39 +0100

    Project initialization

---
+++ src/main/java/com/smalaca/jms/JmsApplication.java
@@ -0,0 +1,7 @@
+package com.smalaca.jms;
+
+public class JmsApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/smalaca/jms/JmsApplicationTest.java
@@ -0,0 +1,4 @@
+package com.smalaca.jms;
+
+public class JmsApplicationTest {
+}
